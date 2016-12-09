074ee0e1c91da62902ef8295cd943c509a8e30cd
Louis Coquio <louis.coquio@gmail.com>
2016-12-09 13:16:14 +0100

    (entry-group) Fix number of days "from now", and future dates

--- app/components/entry-group-day.js
+++ app/components/entry-group-day.js
@@ -6,19 +6,25 @@ const { get } = Ember;
 const EntryGroupDayComponent = Ember.Component.extend({
   day: null,
 
-  isToday: Ember.computed('day', function() {
-    const day = get(this, 'day');
-    return moment().isSame(day, 'day');
-  }),
+  formatted: Ember.computed('isToday', 'isCurrentWeek', 'isCurrentYear', function() {
+    const day = moment(get(this, 'day')).hours(0).minutes(0).seconds(0);
+    const today = moment().hours(0).minutes(0).seconds(0);
+    const yesterday = moment(today.toDate()).subtract(1, 'day');
 
-  isCurrentWeek: Ember.computed('day', function() {
-    const day = get(this, 'day');
-    return moment().isSame(day, 'week');
-  }),
+    if (day.isAfter(today, 'day')) {
+      if (day.isSame(today, 'year')) {
+        return day.format("ddd[,] D MMM");
+      } else {
+        return day.format("ddd[,] D MMM YYYY");
+      }
+    }
 
-  isCurrentYear: Ember.computed('day', function() {
-    const day = get(this, 'day');
-    return moment().isSame(day, 'year');
+    if (day.isSame(today, 'day')) {     return "Today"; }
+    if (day.isSame(yesterday, 'day')) { return "Yesterday"; }
+    if (day.isSame(today, 'week')) {    return day.from(today); }
+    if (day.isSame(today, 'year')) {    return day.format("ddd[,] D MMM"); }
+
+    return day.format("ddd[,] D MMM YYYY");
   })
 });
 
--- tests/integration/components/entry-group-day-test.js
+++ tests/integration/components/entry-group-day-test.js
@@ -16,6 +16,13 @@ test('it is today', function(assert) {
   assert.ok(this.$().text().match(/today/i), 'should be today');
 });
 
+test('it is yesterday', function(assert) {
+  const day = moment().subtract(1, 'day').toDate();
+  set(this, 'day', day);
+  this.render(hbs`{{entry-group-day day}}`);
+  assert.ok(this.$().text().match(/yesterday/i), 'should be yesterday');
+});
+
 test('it is the number of days from now', function(assert) {
   const day = moment().subtract(2, 'days').toDate();
   set(this, 'day', day);
@@ -31,8 +38,23 @@ test('it is the day and month when older than a week but in current year', funct
   assert.notOk(this.$().text().match(day.format("YYYY")), 'should not have year');
 });
 
+test('is is the day and month when the day is in the future but in current year', function(assert) {
+  const day = moment().add(1, 'day');
+  set(this, 'day', day.toDate());
+  this.render(hbs`{{entry-group-day day}}`);
+  assert.ok(this.$().text().match(day.format("ddd[,] D MMM")), 'should be day and month');
+  assert.notOk(this.$().text().match(day.format("YYYY")), 'should not have year');
+});
+
 test('it is the day, month and year when older a year', function(assert) {
   const day = moment().subtract(1, 'year');
+  set(this, 'day', day.toDate());
+  this.render(hbs`{{entry-group-day day}}`);
+  assert.ok(this.$().text().match(day.format("ddd[,] D MMM YYYY")), 'should be day, month and year');
+});
+
+test('is is the day and month when the day is in the future but in next year', function(assert) {
+  const day = moment().add(1, 'year');
   set(this, 'day', day.toDate());
   this.render(hbs`{{entry-group-day day}}`);
   assert.ok(this.$().text().match(day.format("ddd[,] D MMM YYYY")), 'should be day, month and year');
