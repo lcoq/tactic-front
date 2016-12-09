03d48dc6814f1463900622feec2413d46fef12c0
Louis Coquio <louis.coquio@gmail.com>
2016-12-09 16:24:24 +0100

    (entry-list) Prevent recomputing whole entry list when creating/updating/deleting an entry
    It fixes multiple scenarii when some {{show-entry}} timers were executed
    while the component was already destroyed

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -68,6 +68,48 @@ export default Ember.Controller.extend({
     }
   },
 
+
+
+  _findGroupByEntry(groups, entry) {
+    return groups.find(function(group) {
+      return get(group, 'entries').includes(entry);
+    });
+  },
+
+  _removeEntryFromGroupAndPossiblyWholeGroup(groups, group, entry) {
+    if (get(group, 'entries.length') === 1) {
+      groups.removeObject(group);
+    } else {
+      get(group, 'entries').removeObject(entry);
+    }
+  },
+
+  _addEntryToEntriesByDay(entry) {
+    const groups = get(this, 'entriesByDay');
+    const day = moment(get(entry, 'startedAt')).startOf('day').toDate();
+    const group = this._findGroup(groups, day) || this._createGroupAndInsertIn(groups, day);
+    this._addEntry(group, entry);
+  },
+
+  _updateEntryOnEntriesByDay(entry) {
+    const groups = get(this, 'entriesByDay');
+    const day = moment(get(entry, 'startedAt')).startOf('day').toDate();
+
+    const previousGroup = this._findGroupByEntry(groups, entry);
+    const newGroup = this._findGroup(groups, day);
+
+    if (previousGroup !== newGroup) {
+      this._removeEntryFromGroupAndPossiblyWholeGroup(groups, previousGroup, entry);
+      this._addEntryToEntriesByDay(entry);
+    }
+  },
+
+  _removeEntryFromEntriesByDay(entry) {
+    const groups = get(this, 'entriesByDay');
+    const group = this._findGroupByEntry(groups, entry);
+    this._removeEntryFromGroupAndPossiblyWholeGroup(groups, group, entry);
+  },
+
   actions: {
     searchProjects(query) {
       return get(this, 'store').query('project', { filter: { query: query } });
@@ -79,9 +121,8 @@ export default Ember.Controller.extend({
     saveNewEntry() {
       const entry = get(this, 'newEntry');
       entry.save().then(() => {
-        get(this, 'model').pushObject(entry);
+        this._addEntryToEntriesByDay(entry);
         get(this, 'currentWeek').reload();
-        this.notifyPropertyChange('model');
         this.send('buildNewEntry');
       });
     },
@@ -91,15 +132,14 @@ export default Ember.Controller.extend({
       return entry.save().then(() => {
         get(this, 'currentWeek').reload();
         if (dateChanged) {
-          this.notifyPropertyChange('model');
+          this._updateEntryOnEntriesByDay(entry);
         }
       });
     },
     deleteEntry(entry) {
       entry.destroyRecord().then(() => {
-        get(this, 'model').removeObject(entry);
+        this._removeEntryFromEntriesByDay(entry);
         get(this, 'currentWeek').reload();
-        this.notifyPropertyChange('model');
       });
     }
   }
